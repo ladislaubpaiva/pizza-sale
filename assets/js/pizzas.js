3bcaa3457f8f2579c60f79234933fff6450bd0a9
Ladislau Paiva <ladislaubpaiva@hotmail.com>
2022-04-07 00:22:51 +0100

    Add Coupon System

--- assets/js/pizzas.js
+++ assets/js/pizzas.js
@@ -3,7 +3,7 @@ let pizzaJson = [
     id: 1,
     name: 'Rimuru Tempest',
     img: 'assets/images/pizza.png',
-    price: 10.19,
+    price: [7.13, 10.19, 13.25],
     sizes: ['100g', '530g', '860g'],
     description:
       'Description of the pizza in more than one line, very cool very interesting',
@@ -12,7 +12,8 @@ let pizzaJson = [
     id: 2,
     name: 'Lelouch Lamperouge',
     img: 'assets/images/pizza2.png',
-    price: 8.0,
+    price: [5.6, 8.0, 10.4],
+
     sizes: ['320g', '530g', '860g'],
     description:
       'Description of the pizza in more than one line, very cool very interesting',
@@ -21,7 +22,7 @@ let pizzaJson = [
     id: 3,
     name: 'Uzumaki Naruto',
     img: 'assets/images/pizza3.png',
-    price: 7.45,
+    price: [5.21, 7.45, 9.69],
     sizes: ['320g', '530g', '860g'],
     description:
       'Description of the pizza in more than one line, very cool very interesting',
@@ -30,7 +31,7 @@ let pizzaJson = [
     id: 4,
     name: 'Levi Ackerman',
     img: 'assets/images/pizza4.png',
-    price: 9.77,
+    price: [6.84, 9.77, 12.7],
     sizes: ['320g', '530g', '860g'],
     description:
       'Description of the pizza in more than one line, very cool very interesting',
@@ -39,7 +40,8 @@ let pizzaJson = [
     id: 5,
     name: 'Eren Jeager',
     img: 'assets/images/pizza5.png',
-    price: 11.43,
+    price: [8.0, 11.43, 14.86],
+
     sizes: ['160g', '530g', '860g'],
     description:
       'Description of the pizza in more than one line, very cool very interesting',
@@ -48,7 +50,8 @@ let pizzaJson = [
     id: 6,
     name: 'Kyosuke Kyoko',
     img: 'assets/images/pizza6.png',
-    price: 8.55,
+    price: [5.99, 8.55, 11.12],
+
     sizes: ['320g', '530g', '860g'],
     description:
       'Description of the pizza in more than one line, very cool very interesting',
@@ -57,7 +60,7 @@ let pizzaJson = [
     id: 7,
     name: 'Ken Kaneki',
     img: 'assets/images/pizza7.png',
-    price: 12.36,
+    price: [8.65, 12.36, 16.07],
     sizes: ['280g', '530g', '860g'],
     description:
       'Description of the pizza in more than one line, very cool very interesting',
@@ -66,7 +69,7 @@ let pizzaJson = [
     id: 8,
     name: 'Agatsuma Zenitsu',
     img: 'assets/images/pizza8.png',
-    price: 9.36,
+    price: [6.55, 9.36, 12.17],
     sizes: ['180g', '530g', '860g'],
     description:
       'Description of the pizza in more than one line, very cool very interesting',
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
 let modalQt = 1,
-  modalKey = 0,
-  price = 0;
+  modalKey = 0;
 let cart = [];
 const selector = (element) => {
   return document.querySelector(element);
@@ -21,7 +20,7 @@ pizzaJson.map((item, index) => {
   pizzaItem.querySelector('.pizza-item--name').innerText = item.name;
   pizzaItem.querySelector(
     '.pizza-item--price'
-  ).innerText = `$ ${item.price.toFixed(2)}`;
+  ).innerText = `$ ${item.price[1].toFixed(2)}`;
   pizzaItem.querySelector('a').addEventListener('click', (event) => {
     event.preventDefault();
     let key = event.target.closest('.pizza-item').getAttribute('data-key');
@@ -31,8 +30,8 @@ pizzaJson.map((item, index) => {
     selector('.pizzaBig img').setAttribute('draggable', 'false');
     selector('.pizzaInfo h1').innerText = pizzaJson[key].name;
     selector('.pizzaInfo--desc').innerText = pizzaJson[key].description;
-    price = pizzaJson[key].price;
-    selector('.pizzaInfo--actualPrice').innerText = `$${price.toFixed(2)}`;
+    selector('.pizzaInfo--actualPrice').innerText =
+      pizzaJson[key].price[1].toFixed(2);
     selector('.pizzaInfo--size.selected').classList.remove('selected');
     selectorAll('.pizzaInfo--size').forEach((size, index) => {
       if (index == 1) size.classList.add('selected');
@@ -40,15 +39,22 @@ pizzaJson.map((item, index) => {
       size.addEventListener('click', () => {
         selector('.pizzaInfo--size.selected').classList.remove('selected');
         size.classList.add('selected');
-        if (index == 0) {
-          price = (pizzaJson[key].price * 0.7).toFixed(2);
-          selector('.pizzaInfo--actualPrice').innerText = `$${price}`;
-        } else if (index == 1) {
-          price = pizzaJson[key].price.toFixed(2);
-          selector('.pizzaInfo--actualPrice').innerText = `$${price}`;
-        } else {
-          price = (pizzaJson[key].price * 1.3).toFixed(2);
-          selector('.pizzaInfo--actualPrice').innerText = `$${price}`;
+        let sizeSelect = selector('.pizzaInfo--size.selected').getAttribute(
+          'data-key'
+        );
+        switch (Number(sizeSelect)) {
+          case 0:
+            selector('.pizzaInfo--actualPrice').innerText =
+              pizzaJson[key].price[0].toFixed(2);
+            break;
+          case 1:
+            selector('.pizzaInfo--actualPrice').innerText =
+              pizzaJson[key].price[1].toFixed(2);
+            break;
+          case 2:
+            selector('.pizzaInfo--actualPrice').innerText =
+              pizzaJson[key].price[2].toFixed(2);
+            break;
         }
       });
     });
@@ -60,77 +66,145 @@ pizzaJson.map((item, index) => {
   });
   selector('.pizza-area').append(pizzaItem);
 });
+{
+  // || Modal window events
+  function closeModal() {
+    selector('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() => {
+      modal.style.display = 'none';
+    }, 500);
+  }
 
-// || Modal window events
-function closeModal() {
-  selector('.pizzaWindowArea').style.opacity = 0;
-  setTimeout(() => {
-    modal.style.display = 'none';
-  }, 500);
-}
-
-selectorAll('.pizzaInfo--cancelButton,.pizzaInfo--cancelMobileButton').forEach(
-  (item) => {
+  selectorAll(
+    '.pizzaInfo--cancelButton,.pizzaInfo--cancelMobileButton'
+  ).forEach((item) => {
     item.addEventListener('click', closeModal);
-  }
-);
+  });
 
-selector('.pizzaInfo--qtmais').addEventListener('click', () => {
-  modalQt++;
-  selector('.pizzaInfo--qt').innerText = modalQt;
-});
-selector('.pizzaInfo--qtmenos').addEventListener('click', () => {
-  if (modalQt > 1) {
-    modalQt--;
+  selector('.pizzaInfo--qtmais').addEventListener('click', () => {
+    modalQt++;
     selector('.pizzaInfo--qt').innerText = modalQt;
-  }
-});
-
-// || Cart
-selector('.pizzaInfo--addButton').addEventListener('click', () => {
-  let size = selector('.pizzaInfo--size.selected').getAttribute('data-key');
-  let identifier = pizzaJson[modalKey].id + '@' + size;
-  let key = cart.findIndex((item) => item.identifier === identifier);
-  if (key > -1) {
-    cart[key].qt += modalQt;
-  } else {
-    cart.push({
-      identifier,
-      id: pizzaJson[modalKey].id,
-      size,
-      qt: modalQt,
-    });
-  }
-  updateCart();
-  closeModal();
-});
-function updateCart() {
-  if (cart.length > 0) {
-    selector('aside').classList.add('show');
-    selector('.cart').innerText = '';
-    for (let i in cart) {
-      let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id);
-      let cartItem = selector('.models .cart--item').cloneNode(true);
-      cartItem.querySelector('img').src = pizzaItem.img;
-      // console.log(pizzaSizeName);
-      let pizzaSizeName;
-      switch (Number(cart[i].size)) {
-        case 0:
-          pizzaSizeName = 'S';
-          break;
-        case 1:
-          pizzaSizeName = 'M';
-          break;
-        case 2:
-          pizzaSizeName = 'B';
-          break;
-      }
-      let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
-      cartItem.querySelector('.cart--item-name').innerText = pizzaName;
-      cartItem.querySelector('.cart--item--qt').innerText = cart[i].qt;
-      selector('.cart').append(cartItem);
+  });
+  selector('.pizzaInfo--qtmenos').addEventListener('click', () => {
+    if (modalQt > 1) {
+      modalQt--;
+      selector('.pizzaInfo--qt').innerText = modalQt;
     }
-  } else {
-    selector('aside').classList.remove('show');
+  });
+
+  // || Cart
+  selector('.pizzaInfo--addButton').addEventListener('click', () => {
+    let size = selector('.pizzaInfo--size.selected').getAttribute('data-key');
+    let identifier = pizzaJson[modalKey].id + '@' + size;
+    let key = cart.findIndex((item) => item.identifier === identifier);
+    if (key > -1) {
+      cart[key].qt += modalQt;
+    } else {
+      cart.push({
+        identifier,
+        id: pizzaJson[modalKey].id,
+        size,
+        qt: modalQt,
+      });
+    }
+    updateCart();
+    closeModal();
+  });
+  function updateCart() {
+    if (cart.length > 0) {
+      selector('aside').classList.add('show');
+      selector('.cart').innerText = '';
+      let subTotal = 0,
+        discount = 0,
+        total = 0;
+      for (let i in cart) {
+        let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id);
+        let cartItem = selector('.models .cart--item').cloneNode(true);
+        cartItem.querySelector('img').src = pizzaItem.img;
+        let pizzaSizeName;
+        let pizzaPrice;
+        switch (Number(cart[i].size)) {
+          case 0:
+            pizzaSizeName = 'S';
+            pizzaPrice = pizzaItem.price[0].toFixed(2);
+            break;
+          case 1:
+            pizzaSizeName = 'M';
+            pizzaPrice = pizzaItem.price[1].toFixed(2);
+            break;
+          case 2:
+            pizzaSizeName = 'B';
+            pizzaPrice = pizzaItem.price[2].toFixed(2);
+            break;
+        }
+        subTotal += pizzaPrice * cart[i].qt;
+        let pizzaName = `${pizzaItem.name} (${pizzaSizeName})`;
+        cartItem.querySelector('.cart--item-name').innerText = pizzaName;
+        cartItem.querySelector('.cart--item--qt').innerText = cart[i].qt;
+        cartItem
+          .querySelector('.cart--item-qtmais')
+          .addEventListener('click', () => {
+            cart[i].qt++;
+            updateCart();
+          });
+        cartItem
+          .querySelector('.cart--item-qtmenos')
+          .addEventListener('click', () => {
+            if (cart[i].qt > 1) {
+              cart[i].qt--;
+            } else {
+              cart.splice(i, 1);
+            }
+            updateCart();
+          });
+        selector('.cart').append(cartItem);
+      }
+      selector('#promo').addEventListener('keydown', (e) => {
+        let promoCode = selector('#promo').value;
+        let promo = promoCode.toUpperCase();
+        let key = e.key;
+        let priceDiscount = (v) => {
+          discount = (subTotal * v).toFixed(2);
+          console.log(discount);
+        };
+        if (key == 'Enter') {
+          switch (promo) {
+            case 'LADIS':
+              priceDiscount(0.1);
+              total = (subTotal - discount).toFixed(2);
+              selector('span#discount').innerText = `$ ${discount}`;
+              selector('span#total').innerText = `$ ${total}`;
+              break;
+            case 'PAIVA':
+              priceDiscount(0.2);
+              total = (subTotal - discount).toFixed(2);
+              selector('span#discount').innerText = `$ ${discount}`;
+              selector('span#total').innerText = `$ ${total}`;
+              break;
+            case 'ANIME':
+              priceDiscount(0.3);
+              total = (subTotal - discount).toFixed(2);
+              selector('span#discount').innerText = `$ ${discount}`;
+              selector('span#total').innerText = `$ ${total}`;
+              break;
+            default:
+              priceDiscount(0);
+              total = (subTotal - discount).toFixed(2);
+              selector('span#discount').innerText = `$ ${discount}`;
+              selector('span#total').innerText = `$ ${total}`;
+              break;
+          }
+        }
+        // updateCart();
+      });
+      total = (subTotal - discount).toFixed(2);
+      subTotal = subTotal.toFixed(2);
+      selector('.subtotal span:last-child').innerText = `$ ${subTotal}`;
+      selector('span#discount').innerText = `$ ${discount}`;
+      selector('span#total').innerText = `$ ${total}`;
+    } else {
+      selector('aside').classList.remove('show');
+    }
   }
+  console.log('PROMO CODES :"LADIS","PAIVA","ANIME"');
 }
